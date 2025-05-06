fa2dfb1c83bbedb92a1ce0144a87e703547277c2
JoemanJP <edwardmt84@gmail.com>
2025-05-06 14:18:27 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,23 +1,43 @@
 const countries = [
   {
-    name: "United Kingdom",
-    flag: "https://flagcdn.com/gb.svg",
-    hdi: 0.940,
-    life: 82.2,
-    rarity: "0.85%"
+    name: "中國",
+    en_name: "China",
+    flag: "https://flagcdn.com/cn.svg",
+    population_pct: 17.6, // 占世界人口 %
+    hdi: 0.768,
+    life: 78.2
   },
   {
-    name: "India",
+    name: "印度",
+    en_name: "India",
     flag: "https://flagcdn.com/in.svg",
+    population_pct: 17.8,
     hdi: 0.633,
-    life: 70.8,
-    rarity: "17.5%"
+    life: 70.8
   },
   {
-    name: "Taiwan",
-    flag: "https://flagcdn.com/tw.svg",
-    hdi: 0.885,
-    life: 80.4,
-    rarity: "0.3%"
-  }
+    name: "美國",
+    en_name: "United States",
+    flag: "https://flagcdn.com/us.svg",
+    population_pct: 4.3,
+    hdi: 0.921,
+    life: 79.1
+  },
+  {
+    name: "印尼",
+    en_name: "Indonesia",
+    flag: "https://flagcdn.com/id.svg",
+    population_pct: 3.5,
+    hdi: 0.705,
+    life: 71.7
+  },
+  {
+    name: "巴基斯坦",
+    en_name: "Pakistan",
+    flag: "https://flagcdn.com/pk.svg",
+    population_pct: 2.8,
+    hdi: 0.544,
+    life: 67.3
+  },
+  // ...後面還有全世界的
 ];
