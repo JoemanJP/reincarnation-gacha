5584fc7eeb9437fe24ff659b99b3c85586aa1aa2
JoemanJP <edwardmt84@gmail.com>
2025-05-06 14:20:18 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,43 +1,13 @@
 const countries = [
-  {
-    name: "中國",
-    en_name: "China",
-    flag: "https://flagcdn.com/cn.svg",
-    population_pct: 17.6, // 占世界人口 %
-    hdi: 0.768,
-    life: 78.2
-  },
-  {
-    name: "印度",
-    en_name: "India",
-    flag: "https://flagcdn.com/in.svg",
-    population_pct: 17.8,
-    hdi: 0.633,
-    life: 70.8
-  },
-  {
-    name: "美國",
-    en_name: "United States",
-    flag: "https://flagcdn.com/us.svg",
-    population_pct: 4.3,
-    hdi: 0.921,
-    life: 79.1
-  },
-  {
-    name: "印尼",
-    en_name: "Indonesia",
-    flag: "https://flagcdn.com/id.svg",
-    population_pct: 3.5,
-    hdi: 0.705,
-    life: 71.7
-  },
-  {
-    name: "巴基斯坦",
-    en_name: "Pakistan",
-    flag: "https://flagcdn.com/pk.svg",
-    population_pct: 2.8,
-    hdi: 0.544,
-    life: 67.3
-  },
-  // ...後面還有全世界的
+  { name: "印度", en_name: "India", flag: "https://flagcdn.com/in.svg", population_pct: 17.8, hdi: 0.633, life: 70.8 },
+  { name: "中國", en_name: "China", flag: "https://flagcdn.com/cn.svg", population_pct: 17.6, hdi: 0.768, life: 78.2 },
+  { name: "美國", en_name: "United States", flag: "https://flagcdn.com/us.svg", population_pct: 4.3, hdi: 0.921, life: 79.1 },
+  { name: "印尼", en_name: "Indonesia", flag: "https://flagcdn.com/id.svg", population_pct: 3.5, hdi: 0.705, life: 71.7 },
+  { name: "巴基斯坦", en_name: "Pakistan", flag: "https://flagcdn.com/pk.svg", population_pct: 2.8, hdi: 0.544, life: 67.3 },
+  { name: "奈及利亞", en_name: "Nigeria", flag: "https://flagcdn.com/ng.svg", population_pct: 2.7, hdi: 0.535, life: 60.9 },
+  { name: "巴西", en_name: "Brazil", flag: "https://flagcdn.com/br.svg", population_pct: 2.6, hdi: 0.754, life: 75.9 },
+  { name: "孟加拉", en_name: "Bangladesh", flag: "https://flagcdn.com/bd.svg", population_pct: 2.1, hdi: 0.661, life: 72.4 },
+  { name: "俄羅斯", en_name: "Russia", flag: "https://flagcdn.com/ru.svg", population_pct: 1.8, hdi: 0.822, life: 72.7 },
+  { name: "墨西哥", en_name: "Mexico", flag: "https://flagcdn.com/mx.svg", population_pct: 1.6, hdi: 0.758, life: 75.0 },
+  { name: "台灣", en_name: "Taiwan", flag: "https://flagcdn.com/tw.svg", population_pct: 0.3, hdi: 0.885, life: 80.4 }
 ];
